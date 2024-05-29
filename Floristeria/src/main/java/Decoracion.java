60a19fbfbf8d4f78b0ddcb29e49d78821460c616
Marc Rodriguez <marcrv1@gmail.com>
2024-05-29 11:04:54 +0200

    prueba1

--- Floristeria/src/main/java/Decoracion.java
+++ Floristeria/src/main/java/Decoracion.java
@@ -3,4 +3,11 @@ public class Decoracion extends Producto{
     public Decoracion(String name) {
         super(name);
     }
+
+    @Override
+    public String toString() {
+        return "Decoracion{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
