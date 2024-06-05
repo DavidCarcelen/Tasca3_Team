9f66cba04c4bc1811378165632f563449f763398
Marc Rodriguez <marcrv1@gmail.com>
2024-06-05 20:26:29 +0200

    queryGenerator

--- Floristeria/src/main/java/Main.java
+++ Floristeria/src/main/java/Main.java
@@ -1,12 +1,10 @@
-import Connections.Database;
 import Menu.Menu;
 
 public class Main {
     public static void main(String[] args) {
-        Database database = Database.getInstance();
+
         Menu menu = new Menu();
         menu.menu();
-        Database.closeDatabase();
 
         //si existe database podemos database.conexion(metodoquesea)
     }
