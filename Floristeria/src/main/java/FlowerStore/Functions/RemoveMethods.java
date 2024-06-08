93151d3a79bc82983f273fe5a7e7fc79fceb5a1a
David Carcelen <davidcarcelen@MacBook-Pro-de-David-2.local>
2024-06-08 09:20:54 +0200

    showCompleto

--- Floristeria/src/main/java/FlowerStore/Functions/RemoveMethods.java
+++ Floristeria/src/main/java/FlowerStore/Functions/RemoveMethods.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 import static Menu.Menu.numCheck;
 
-public class removeMethods {
+public class RemoveMethods {
     //escoger que tipo de producto vamos a eliminar
     //imprimir todos los productos de ese tipo
     //eliminar el producto con un int de id
